1cd75a8d7690e66d2758f2ac36a41392ee14e7b7
binhnqit <binhnqit@gmail.com>
2026-02-05 16:46:58 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+APP_NAME = "4ORANGES - QUẢN LÝ THIẾT BỊ"
+ORANGE_PALETTE = ["#FF8C00", "#FFA500", "#FF4500", "#E67E22", "#D35400"]
+URLS = {
+    "LOI": "https://docs.google.com/spreadsheets/d/e/2PACX-1vS-UP5WFVE63byPckNy_lsT9Rys84A8pPq6cm6rFFBbOnPAsSl1QDLS_A9E45oytg/pub?gid=675485241&single=true&output=csv"
+}
